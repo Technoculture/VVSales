4d28fe747239351b627ec84dada09d79c633aa04
Satyam Tiwary <satyam@technoculture.io>
2023-11-17 00:40:02 +0530

    init

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,8 +3,8 @@ import EditScreenInfo from '../../components/EditScreenInfo';
 
 export default function TabOneScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className='text-white'>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-white text-red-300">
+      <Text className='text-red-200 text-xl'>Open up App.js to start working on your app!</Text>
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
