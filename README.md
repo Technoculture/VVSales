1ba844939328b3c2f9e3b33b04ef8b09e31c7c90
Satyam Tiwary <satyam@technoculture.io>
2023-11-18 11:42:13 +0530

    broken

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Ella
+
+# TODOs
+
+## Getting Started Tasks
+
+- Mic Recording, Store Recordings, Play Recordings
+- Take photo
+- Pick photo from gallary
+- Pick file(pdfs only) from Files on phone
+- Connect to OpenAI APIs - Whisper, GPT3, TTS
+- Add sqlite db / lanceDB?
+- Add function Calling
+
+## Basic Features
+
+- Add a
+
+# Build
+
+```sh
+yarn run ios --clear
+```
+
+# Troubleshooting
+
+```txt
+watchman watch-del 'project location/Ella' ; watchman watch-project 'project location/Ella'
+```
--- app/home.tsx
+++ app/home.tsx
@@ -1,29 +1,48 @@
+import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { Platform, TouchableOpacity } from "react-native";
+import { Camera, CameraType } from "expo-camera";
+import { Audio } from "expo-av";
+import { WaveSurfer } from "wavesurfer-react";
 
 import { Text, View } from "../components/Themed";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 import OpenAI from "openai";
-import { useState } from "react";
+import lancedb from "vectordb";
+
+async function search(vector) {
+  console.log(lancedb);
+  const db = await lancedb.connect("data/sample-lancedb");
+
+  const table = await db.createTable("vectors", [
+    { id: 1, vector: [0.1, 0.2], item: "foo", price: 10 },
+    { id: 2, vector: [1.1, 1.2], item: "bar", price: 50 },
+  ]);
+
+  const query = table.search(vector).limit(2);
+  const results = await query.execute();
+
+  console.log(results);
+  return results;
+}
 
 const openai = new OpenAI({
   apiKey: process.env["OPENAI_API_KEY"],
 });
 
-interface CallButtonProps {
-  trigger: () => {};
+interface RoundedButtonProps {
+  trigger: () => void;
+  icon: string;
 }
 
-export function CallButton(props: CallButtonProps) {
-  const { trigger } = props;
-
+export function RoundedButton({ trigger, icon }: RoundedButtonProps) {
   return (
     <TouchableOpacity
       className="bg-green-600 h-20 w-20 rounded-full items-center justify-center shadow-inner"
       onPress={trigger}
     >
-      <Ionicons name="call" size={32} color="white" className="shadow-2xl" />
+      <Ionicons name={icon} size={32} color="white" className="shadow-2xl" />
     </TouchableOpacity>
   );
 }
@@ -31,10 +50,11 @@ export function CallButton(props: CallButtonProps) {
 interface MessageBlobType {
   type: "human" | "ai";
   text?: string;
+  audio?: string;
 }
 
 export function MessageBlob(props: MessageBlobType) {
-  const { type, text } = props;
+  const { type, text, audio } = props;
 
   const baseStyle = "py-2 px-4 rounded-full mb-2";
   let variantStyle = " ";
@@ -46,18 +66,23 @@ export function MessageBlob(props: MessageBlobType) {
 
   return (
     <View className={`${baseStyle} ${variantStyle}`}>
-      <Text>{text}</Text>
+      {audio != "" ? <WaveSurfer /> : <Text>{text}</Text>}
     </View>
   );
 }
 
 let messages: MessageBlobType[] = [
-  { type: "human", text: "Hi" },
+  { type: "human", text: "Hi", audio: "filename.mp3" },
   { type: "ai", text: "Hi Human!" },
   { type: "human", text: "Nice to meet you" },
 ];
 
 export default function HomeScreen() {
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  const [sound, setSound] = useState();
+
   const [msgs, setMsgs] = useState(messages);
 
   async function talk() {
@@ -79,6 +104,12 @@ export default function HomeScreen() {
     }
   }
 
+  function toggleCameraType() {
+    setType((current) =>
+      current === CameraType.back ? CameraType.front : CameraType.back,
+    );
+  }
+
   return (
     <View className="flex-1">
       <View className="flex-3 items-center justify-center gap-6 bg-green-100">
@@ -92,8 +123,22 @@ export default function HomeScreen() {
         </View>
         <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
       </View>
-      <View className="bg-green-300 flex-1 flex-row gap-2 items-center justify-center shadow-2xl">
-        <CallButton trigger={async () => await talk()} />
+      <View className="bg-green-300 flex-1 flex-row items-center justify-center shadow-2xl">
+        <RoundedButton
+          icon="add-sharp"
+          size="small"
+          trigger={async () => await search([0.1, 0.3])}
+        />
+        <RoundedButton
+          icon="call"
+          size="default"
+          trigger={async () => await talk()}
+        />
+        <RoundedButton
+          icon="camera"
+          size="small"
+          trigger={() => console.log("Camera")}
+        />
       </View>
     </View>
   );
