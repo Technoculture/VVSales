b4d42966bff0037a275c2794ce188fcfe69b3990
Naveen <naveengummella.rg@gmail.com>
2024-01-02 15:40:34 +0530

    trying refcatoring call log

--- app/(tabs)/logs.tsx
+++ app/(tabs)/logs.tsx
@@ -15,7 +15,6 @@ export default function TabTwoScreen() {
   const [callLogs, setCallLogs] = useState<CallLogItem[]>([]);
 
   useEffect(() => {
-    // Function to format call duration
     const formatCallDuration = (duration: string): string => {
       const seconds = parseInt(duration, 10);
       const hours = Math.floor(seconds / 3600);
@@ -40,7 +39,6 @@ export default function TabTwoScreen() {
       return formattedDuration.trim();
     };
 
-    // Function to fetch call logs for the current day
     const fetchCallLogs = async () => {
       try {
         const granted = await PermissionsAndroid.request(
@@ -65,26 +63,25 @@ export default function TabTwoScreen() {
             0,
           );
 
-          CallLogs.load(100).then((logs) => {
-            console.log("Call logs:", logs);
+          const logs = await CallLogs.load(100); // Load more logs for flexibility
 
-            const formattedLogs: CallLogItem[] = logs
-              .filter((log) => new Date(log.timestamp) >= startOfDay)
-              .map((log) => {
-                // Log the entire 'log' object to inspect its structure
-                console.log("Call log structure:", log);
+          console.log("Call logs:", logs);
 
-                return {
-                  phoneNumber: log.phoneNumber,
-                  callType: log.callType,
-                  callDate: new Date(log.timestamp).toLocaleString(),
-                  callDuration: formatCallDuration(log.duration),
-                };
-              });
+          const formattedLogs: CallLogItem[] = logs
+            .filter((log) => new Date(log.timestamp) >= startOfDay)
+            .map((log) => {
+              console.log("Call log structure:", log);
 
-            console.log("Formatted logs:", formattedLogs);
-            setCallLogs(formattedLogs);
-          });
+              return {
+                phoneNumber: log.phoneNumber,
+                callType: log.callType,
+                callDate: new Date(log.timestamp).toLocaleString(),
+                callDuration: formatCallDuration(log.duration),
+              };
+            });
+
+          console.log("Formatted logs:", formattedLogs);
+          setCallLogs(formattedLogs);
         } else {
           console.log("Call Log permission denied");
         }
@@ -93,7 +90,6 @@ export default function TabTwoScreen() {
       }
     };
 
-    // Fetch call logs when the component mounts
     fetchCallLogs();
   }, []);
 
@@ -105,7 +101,6 @@ export default function TabTwoScreen() {
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
-      {/* FlatList to render call logs */}
       <FlatList
         data={callLogs}
         keyExtractor={(_, index) => index.toString()}
